ff57bd3325a268f8646aa91adabda2bd24cf4c4d
Evan Allen <evanallen13@gmail.com>
2026-03-25 13:52:48 +0000

    move set to get

--- get_creds.py
+++ get_creds.py
@@ -1,4 +1,5 @@
 import os
+import re
 import requests
 from PIL import Image
 
@@ -42,6 +43,32 @@ def change_image_size(image_path, size=(256, 256)):
         img = img.resize(size)
         img.save(image_path)
         
+        
+def set_creds():
+    imgs = ""
+    badges = sorted(os.listdir("badges"))
+    for badge in badges:
+        print(f"Processing badge image {badge}")
+        img = f"    <img src='badges/{badge}' alt='{badge}' height='90'>"
+        imgs += img + "\n"
+
+    div = f"""
+    <div id="certifications" align="left">\n{imgs}</div>
+    """
+
+    with open("README.md", "r", encoding="utf-8") as f:
+        readme = f.read()
+
+    pattern = r'<div id="certifications" align="left">[\s\S]*?</div>'
+    updated_readme, replacements = re.subn(pattern, div.strip(), readme, count=1)
+
+    if replacements == 0:
+        print("No certifications div found. Appending to README.md")
+        updated_readme = readme.rstrip() + "\n\n" + div.strip() + "\n"
+
+    with open("README.md", "w", encoding="utf-8") as f:
+        f.write(updated_readme)
+        
 username="evan_allen"
 badges=fetch_org_badges(username)
 print(badges[0])
@@ -51,3 +78,5 @@ for badge in badges:
     if image_path:
         print(f"Downloaded badge image to {image_path}")
         change_image_size(image_path)
+        
+set_creds()
